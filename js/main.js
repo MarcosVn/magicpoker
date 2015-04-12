bd1a7501cd1debf50658776f0bafa725d06de6e2
MarcosVn <marcos.vnaraujo@gmail.com>
2015-04-12 02:04:46 -0300

    Adicionando jquery-ui e montado css para mesa do jogador

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+function betsController ($scope) {
+  $scope.bets = [ {
+      color: 'blue',
+      val: 10,
+    }
+  ];
+
+  
+  $scope.all = function () {
+    var all = 0;
+
+    for (var i = 0; i < $scope.bets.length; i++) {
+        all += $scope.bets[i].val;
+    };
+
+    return all;
+  };
+}
+
+
+$(document).ready(function () {
+  $('.card').hide();
+  $('.bet').hide();
+  $('span').hide();
+
+  $('#start').click(function () {
+      $('.card').draggable();
+      $('.bet').draggable();
+      $('body').addClass('background');
+      $('.bet').show("slow");
+      $('span').show("slow");
+      $('.card').show("slow");
+  });
+
+  $('#end').click(function(){
+    $('.card').hide();
+    $('.bet').hide();
+    $('body').removeClass('background');
+  });
+});
+
+
+
+
+
+
+
+
+
 var sense = sense.init();
     var socket = io();
     var colors = [
@@ -7,6 +56,15 @@ var sense = sense.init();
       "#FFBB33",
       "#FF4444"
     ];
+
+    var cards = [
+
+
+    ];
+
+
+
+
     var selectedColor = colors[0];
     sense.flick(function(data){
       broadcast();
@@ -23,6 +81,7 @@ var sense = sense.init();
     });
     function broadcast(){
       socket.emit('color', selectedColor)
+      //socket.emit('card', selectedCard)
     }
     socket.on('color-broadcast', function(color){
       selectedColor = color;
